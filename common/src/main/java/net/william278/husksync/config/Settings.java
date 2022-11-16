7fe561814fe1cb1aecc9594717038350820012c2
AlexDev_ <alessandrodalfovo2003@gmail.com>
2022-11-16 17:35:20 +0100

    TableName enums in Settings.java had wrong defaultName. I changed them to the correct ones. Added 1 sec delay for join inventory loading system.

---
+++ common/src/main/java/net/william278/husksync/config/Settings.java
@@ -0,0 +1,250 @@
+package net.william278.husksync.config;
+
+import net.william278.annotaml.YamlComment;
+import net.william278.annotaml.YamlFile;
+import net.william278.annotaml.YamlKey;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.Map;
+
+/**
+ * Plugin settings, read from config.yml
+ */
+@YamlFile(header = """
+        ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+        ┃        HuskSync Config       ┃
+        ┃    Developed by William278   ┃
+        ┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+        ┣╸ Information: https://william278.net/project/husksync
+        ┗╸ Documentation: https://william278.net/docs/husksync""",
+        versionField = "config_version", versionNumber = 3)
+public class Settings {
+
+    // Top-level settings
+    public String language = "en-gb";
+
+    @YamlKey("check_for_updates")
+    public boolean checkForUpdates = true;
+
+    @YamlKey("cluster_id")
+    public String clusterId = "";
+
+    @YamlKey("debug_logging")
+    public boolean debugLogging = false;
+
+
+    // Database settings
+    @YamlComment("Database connection settings")
+    @YamlKey("database.credentials.host")
+    public String mySqlHost = "localhost";
+
+    @YamlKey("database.credentials.port")
+    public int mySqlPort = 3306;
+
+    @YamlKey("database.credentials.database")
+    public String mySqlDatabase = "HuskSync";
+
+    @YamlKey("database.credentials.username")
+    public String mySqlUsername = "root";
+
+    @YamlKey("database.credentials.password")
+    public String mySqlPassword = "pa55w0rd";
+
+    @YamlKey("database.credentials.parameters")
+    public String mySqlConnectionParameters = "?autoReconnect=true&useSSL=false";
+
+    @YamlComment("MySQL connection pool properties")
+    @YamlKey("database.connection_pool.maximum_pool_size")
+    public int mySqlConnectionPoolSize = 10;
+
+    @YamlKey("database.connection_pool.minimum_idle")
+    public int mySqlConnectionPoolIdle = 10;
+
+    @YamlKey("database.connection_pool.maximum_lifetime")
+    public long mySqlConnectionPoolLifetime = 1800000;
+
+    @YamlKey("database.connection_pool.keepalive_time")
+    public long mySqlConnectionPoolKeepAlive = 0;
+
+    @YamlKey("database.connection_pool.connection_timeout")
+    public long mySqlConnectionPoolTimeout = 5000;
+
+    @YamlKey("database.table_names")
+    public Map<String, String> tableNames = TableName.getDefaults();
+
+    @NotNull
+    public String getTableName(@NotNull TableName tableName) {
+        return tableNames.getOrDefault(tableName.name().toLowerCase(), tableName.defaultName);
+    }
+
+
+    // Redis settings
+    @YamlComment("Redis connection settings")
+    @YamlKey("redis.credentials.host")
+    public String redisHost = "localhost";
+
+    @YamlKey("redis.credentials.port")
+    public int redisPort = 6379;
+
+    @YamlKey("redis.credentials.password")
+    public String redisPassword = "";
+
+    @YamlKey("redis.use_ssl")
+    public boolean redisUseSsl = false;
+
+
+    // Synchronization settings
+    @YamlComment("Synchronization settings")
+    @YamlKey("synchronization.max_user_data_snapshots")
+    public int maxUserDataSnapshots = 5;
+
+    @YamlKey("synchronization.save_on_world_save")
+    public boolean saveOnWorldSave = true;
+
+    @YamlKey("synchronization.save_on_death")
+    public boolean saveOnDeath = false;
+
+    @YamlKey("synchronization.compress_data")
+    public boolean compressData = true;
+
+    @YamlKey("synchronization.save_dead_player_inventories")
+    public boolean saveDeadPlayerInventories = true;
+
+    @YamlKey("synchronization.network_latency_milliseconds")
+    public int networkLatencyMilliseconds = 500;
+
+    @YamlKey("synchronization.features")
+    public Map<String, Boolean> synchronizationFeatures = SynchronizationFeature.getDefaults();
+
+    public boolean getSynchronizationFeature(@NotNull SynchronizationFeature feature) {
+        return synchronizationFeatures.getOrDefault(feature.name().toLowerCase(), feature.enabledByDefault);
+    }
+
+    @YamlKey("synchronization.event_priorities")
+    public Map<String, String> synchronizationEventPriorities = EventType.getDefaults();
+
+    @NotNull
+    public EventPriority getEventPriority(@NotNull Settings.EventType eventType) {
+        try {
+            return EventPriority.valueOf(synchronizationEventPriorities.get(eventType.name().toLowerCase()));
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            return EventPriority.NORMAL;
+        }
+    }
+
+
+    /**
+     * Represents the names of tables in the database
+     */
+    public enum TableName {
+        USERS("players"),
+        USER_DATA("user_data");
+
+        private final String defaultName;
+
+        TableName(@NotNull String defaultName) {
+            this.defaultName = defaultName;
+        }
+
+        @NotNull
+        private Map.Entry<String, String> toEntry() {
+            return Map.entry(name().toLowerCase(), defaultName);
+        }
+
+        @SuppressWarnings("unchecked")
+        @NotNull
+        private static Map<String, String> getDefaults() {
+            return Map.ofEntries(Arrays.stream(values())
+                    .map(TableName::toEntry)
+                    .toArray(Map.Entry[]::new));
+        }
+    }
+
+    /**
+     * Represents enabled synchronisation features
+     */
+    public enum SynchronizationFeature {
+        INVENTORIES(true),
+        ENDER_CHESTS(true),
+        HEALTH(true),
+        MAX_HEALTH(true),
+        HUNGER(true),
+        EXPERIENCE(true),
+        POTION_EFFECTS(true),
+        ADVANCEMENTS(true),
+        GAME_MODE(true),
+        STATISTICS(true),
+        PERSISTENT_DATA_CONTAINER(false),
+        LOCKED_MAPS(true),
+        LOCATION(false);
+
+        private final boolean enabledByDefault;
+
+        SynchronizationFeature(boolean enabledByDefault) {
+            this.enabledByDefault = enabledByDefault;
+        }
+
+        @NotNull
+        private Map.Entry<String, Boolean> toEntry() {
+            return Map.entry(name().toLowerCase(), enabledByDefault);
+        }
+
+        @SuppressWarnings("unchecked")
+        @NotNull
+        private static Map<String, Boolean> getDefaults() {
+            return Map.ofEntries(Arrays.stream(values())
+                    .map(SynchronizationFeature::toEntry)
+                    .toArray(Map.Entry[]::new));
+        }
+    }
+
+    /**
+     * Represents events that HuskSync listens to, with a configurable priority listener
+     */
+    public enum EventType {
+        JOIN_LISTENER(EventPriority.LOWEST),
+        QUIT_LISTENER(EventPriority.LOWEST),
+        DEATH_LISTENER(EventPriority.NORMAL);
+
+        private final EventPriority defaultPriority;
+
+        EventType(@NotNull EventPriority defaultPriority) {
+            this.defaultPriority = defaultPriority;
+        }
+
+        @NotNull
+        private Map.Entry<String, String> toEntry() {
+            return Map.entry(name().toLowerCase(), defaultPriority.name());
+        }
+
+
+        @SuppressWarnings("unchecked")
+        @NotNull
+        private static Map<String, String> getDefaults() {
+            return Map.ofEntries(Arrays.stream(values())
+                    .map(EventType::toEntry)
+                    .toArray(Map.Entry[]::new));
+        }
+    }
+
+    /**
+     * Represents priorities for events that HuskSync listens to
+     */
+    public enum EventPriority {
+        /**
+         * Listens and processes the event execution last
+         */
+        HIGHEST,
+        /**
+         * Listens in between {@link #HIGHEST} and {@link #LOWEST} priority marked
+         */
+        NORMAL,
+        /**
+         * Listens and processes the event execution first
+         */
+        LOWEST
+    }
+
+}
---
+++ common/src/main/java/net/william278/husksync/listener/EventListener.java
@@ -0,0 +1,227 @@
+package net.william278.husksync.listener;
+
+import net.william278.husksync.HuskSync;
+import net.william278.husksync.data.DataSaveCause;
+import net.william278.husksync.data.ItemData;
+import net.william278.husksync.player.OnlineUser;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Level;
+
+/**
+ * Handles what should happen when events are fired
+ */
+public abstract class EventListener {
+
+    /**
+     * The plugin instance
+     */
+    protected final HuskSync plugin;
+
+    /**
+     * Set of UUIDs of "locked players", for which events will be cancelled.
+     * </p>
+     * Players are locked while their items are being set (on join) or saved (on quit)
+     */
+    private final Set<UUID> lockedPlayers;
+
+    /**
+     * Whether the plugin is currently being disabled
+     */
+    private boolean disabling;
+
+    protected EventListener(@NotNull HuskSync plugin) {
+        this.plugin = plugin;
+        this.lockedPlayers = new HashSet<>();
+        this.disabling = false;
+    }
+
+    /**
+     * Handle a player joining the server (including players switching from another proxied server)
+     *
+     * @param user The {@link OnlineUser} to handle
+     */
+    protected final void handlePlayerJoin(@NotNull OnlineUser user) {
+        lockedPlayers.add(user.uuid);
+        CompletableFuture.runAsync(() -> {
+            try {
+                // Hold reading data for the network latency threshold, to ensure the source server has set the redis key
+                Thread.sleep(Math.max(0, plugin.getSettings().networkLatencyMilliseconds));
+            } catch (InterruptedException e) {
+                plugin.getLoggingAdapter().log(Level.SEVERE, "An exception occurred handling a player join", e);
+            } finally {
+                plugin.getRedisManager().getUserServerSwitch(user).thenAccept(changingServers -> {
+                    if (!changingServers) {
+                        // Fetch from the database if the user isn't changing servers
+                        setUserFromDatabase(user).thenAccept(succeeded -> handleSynchronisationCompletion(user, succeeded));
+                    } else {
+                        final int TIME_OUT_MILLISECONDS = 3200;
+                        CompletableFuture.runAsync(() -> {
+                            final AtomicInteger currentMilliseconds = new AtomicInteger(0);
+                            final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+
+                            // Set the user as soon as the source server has set the data to redis
+                            executor.scheduleAtFixedRate(() -> {
+                                if (user.isOffline()) {
+                                    executor.shutdown();
+                                    return;
+                                }
+                                if (disabling || currentMilliseconds.get() > TIME_OUT_MILLISECONDS) {
+                                    executor.shutdown();
+                                    setUserFromDatabase(user).thenAccept(
+                                            succeeded -> handleSynchronisationCompletion(user, succeeded));
+                                    return;
+                                }
+                                plugin.getRedisManager().getUserData(user).thenAccept(redisUserData ->
+                                        redisUserData.ifPresent(redisData -> {
+                                            user.setData(redisData, plugin.getSettings(), plugin.getEventCannon(),
+                                                            plugin.getLoggingAdapter(), plugin.getMinecraftVersion())
+                                                    .thenAccept(succeeded -> handleSynchronisationCompletion(user, succeeded)).join();
+                                            executor.shutdown();
+                                        })).join();
+                                currentMilliseconds.addAndGet(200);
+                            }, 0, 200L, TimeUnit.MILLISECONDS);
+                        });
+                    }
+                });
+            }
+        }, CompletableFuture.delayedExecutor(1000, TimeUnit.MILLISECONDS));
+    }
+
+    /**
+     * Set a user's data from the database
+     *
+     * @param user The user to set the data for
+     * @return Whether the data was successfully set
+     */
+    private CompletableFuture<Boolean> setUserFromDatabase(@NotNull OnlineUser user) {
+        return plugin.getDatabase().getCurrentUserData(user).thenApply(databaseUserData -> {
+            if (databaseUserData.isPresent()) {
+                return user.setData(databaseUserData.get().userData(), plugin.getSettings(), plugin.getEventCannon(),
+                        plugin.getLoggingAdapter(), plugin.getMinecraftVersion()).join();
+            }
+            return true;
+        });
+    }
+
+    /**
+     * Handle a player's synchronization completion
+     *
+     * @param user      The {@link OnlineUser} to handle
+     * @param succeeded Whether the synchronization succeeded
+     */
+    private void handleSynchronisationCompletion(@NotNull OnlineUser user, boolean succeeded) {
+        if (succeeded) {
+            plugin.getLocales().getLocale("synchronisation_complete").ifPresent(user::sendActionBar);
+            plugin.getDatabase().ensureUser(user).join();
+            lockedPlayers.remove(user.uuid);
+            plugin.getEventCannon().fireSyncCompleteEvent(user);
+        } else {
+            plugin.getLocales().getLocale("synchronisation_failed")
+                    .ifPresent(user::sendMessage);
+            plugin.getDatabase().ensureUser(user).join();
+        }
+    }
+
+    /**
+     * Handle a player leaving the server (including players switching to another proxied server)
+     *
+     * @param user The {@link OnlineUser} to handle
+     */
+    protected final void handlePlayerQuit(@NotNull OnlineUser user) {
+        // Players quitting have their data manually saved by the plugin disable hook
+        if (disabling) {
+            return;
+        }
+        // Don't sync players awaiting synchronization
+        if (lockedPlayers.contains(user.uuid)) {
+            return;
+        }
+
+        // Handle asynchronous disconnection
+        lockedPlayers.add(user.uuid);
+        CompletableFuture.runAsync(() -> plugin.getRedisManager().setUserServerSwitch(user)
+                .thenRun(() -> user.getUserData(plugin.getLoggingAdapter(), plugin.getSettings()).thenAccept(
+                        optionalUserData -> optionalUserData.ifPresent(userData -> plugin.getRedisManager()
+                                .setUserData(user, userData).thenRun(() -> plugin.getDatabase()
+                                        .setUserData(user, userData, DataSaveCause.DISCONNECT)))))
+                .exceptionally(throwable -> {
+                    plugin.getLoggingAdapter().log(Level.SEVERE,
+                            "An exception occurred handling a player disconnection");
+                    throwable.printStackTrace();
+                    return null;
+                }).join());
+    }
+
+    /**
+     * Handles the saving of data when the world save event is fired
+     *
+     * @param usersInWorld a list of users in the world that is being saved
+     */
+    protected final void saveOnWorldSave(@NotNull List<OnlineUser> usersInWorld) {
+        if (disabling || !plugin.getSettings().saveOnWorldSave) {
+            return;
+        }
+        usersInWorld.forEach(user -> user.getUserData(plugin.getLoggingAdapter(), plugin.getSettings()).join().ifPresent(
+                userData -> plugin.getDatabase().setUserData(user, userData, DataSaveCause.WORLD_SAVE).join()));
+    }
+
+    /**
+     * Handles the saving of data when a player dies
+     *
+     * @param user  The user who died
+     * @param drops The items that this user would have dropped
+     */
+    protected void saveOnPlayerDeath(@NotNull OnlineUser user, @NotNull ItemData drops) {
+        if (disabling || !plugin.getSettings().saveOnDeath) {
+            return;
+        }
+
+        user.getUserData(plugin.getLoggingAdapter(), plugin.getSettings())
+                .thenAccept(data -> data.ifPresent(userData -> {
+                    userData.getInventory().orElse(ItemData.empty()).serializedItems = drops.serializedItems;
+                    plugin.getDatabase().setUserData(user, userData, DataSaveCause.DEATH);
+                }));
+    }
+
+    /**
+     * Determine whether a player event should be cancelled
+     *
+     * @param userUuid The UUID of the user to check
+     * @return Whether the event should be cancelled
+     */
+    protected final boolean cancelPlayerEvent(@NotNull UUID userUuid) {
+        return disabling || lockedPlayers.contains(userUuid);
+    }
+
+    /**
+     * Handle the plugin disabling
+     */
+    public final void handlePluginDisable() {
+        disabling = true;
+
+        // Save data for all online users
+        plugin.getOnlineUsers().stream()
+                .filter(user -> !lockedPlayers.contains(user.uuid))
+                .forEach(user -> {
+                    lockedPlayers.add(user.uuid);
+                    user.getUserData(plugin.getLoggingAdapter(), plugin.getSettings()).join()
+                            .ifPresent(userData -> plugin.getDatabase()
+                                    .setUserData(user, userData, DataSaveCause.SERVER_SHUTDOWN).join());
+                });
+
+        // Close outstanding connections
+        plugin.getDatabase().close();
+        plugin.getRedisManager().close();
+    }
+
+}
